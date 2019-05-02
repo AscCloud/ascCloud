b1a093e4e77cc528e579c3a6551b52398f47e327
Idsanchez0 <33404897+Idsanchez0@users.noreply.github.com>
2019-05-02 09:24:14 -0500

    pruebas

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '192.168.100.9'),
+            'host' => env('DB_HOST', '192.168.100.11'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
