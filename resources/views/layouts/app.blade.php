bd825fda805b379909fbbd8ed3fd7e95d57a70f3
Idsanchez0 <33404897+Idsanchez0@users.noreply.github.com>
2019-05-20 11:03:02 -0500

    infyom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>InfyOm Generator</title>
+    <title></title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <!-- Bootstrap 3.3.7 -->
@@ -46,7 +46,7 @@
 
             <!-- Logo -->
             <a href="#" class="logo">
-                <b>InfyOm</b>
+                <b></b>
             </a>
 
             <!-- Header Navbar -->
@@ -126,7 +126,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{!! url('/') !!}">
-                    InfyOm Generator
+
                 </a>
             </div>
 
